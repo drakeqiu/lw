a76d01b5fe430c9c8e42efe89184cfe28528deb0
U-SFIT\qiu.yuan <qiu.yuan@ZJ-OA-QIU-Y2.sfit.shfe.com.cn>
2016-07-28 09:39:36 +0800

    [lw] update domain properties

--- lw-aggregator/lw-core/src/main/java/com/jarvis/lw/core/domain/Account.java
+++ lw-aggregator/lw-core/src/main/java/com/jarvis/lw/core/domain/Account.java
@@ -3,5 +3,50 @@ package com.jarvis.lw.core.domain;
 import java.io.Serializable;
 
 public class Account implements Serializable {
+	private Long id;
+	private String username;
+	private String password;
+	private Boolean isActivated;
+	private Long modifyDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getIsActivated() {
+		return isActivated;
+	}
+
+	public void setIsActivated(Boolean isActivated) {
+		this.isActivated = isActivated;
+	}
+
+	public Long getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Long modifyDate) {
+		this.modifyDate = modifyDate;
+	}
 
 }
--- lw-aggregator/lw-core/src/main/java/com/jarvis/lw/core/domain/User.java
+++ lw-aggregator/lw-core/src/main/java/com/jarvis/lw/core/domain/User.java
@@ -7,5 +7,55 @@ public class User implements Serializable {
 	private String name;
 	private String cell;
 	private String address;
-	
+	private Long birthday;
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCell() {
+		return cell;
+	}
+
+	public void setCell(String cell) {
+		this.cell = cell;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Long birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
