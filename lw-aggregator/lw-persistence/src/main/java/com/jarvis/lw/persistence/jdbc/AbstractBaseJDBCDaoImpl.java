4c56e8f4974f944f1645b76e03ab401635365d67
Drake Qiu <adidasqiu@hotmail.com>
2016-07-29 06:43:05 +0800

    [lw] update user db operation

--- lw-aggregator/lw-persistence/src/main/java/com/jarvis/lw/persistence/jdbc/AbstractBaseJDBCDaoImpl.java
+++ lw-aggregator/lw-persistence/src/main/java/com/jarvis/lw/persistence/jdbc/AbstractBaseJDBCDaoImpl.java
@@ -1,18 +1,23 @@
 package com.jarvis.lw.persistence.jdbc;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.jarvis.lw.core.constants.BaseConstant;
 import com.jarvis.lw.core.constants.BaseStatus;
-import com.jarvis.lw.persistence.AbstractBaseDaoImpl;
+import com.jarvis.lw.persistence.IBaseDao;
 
-public abstract class AbstractBaseJDBCDaoImpl<T> extends AbstractBaseDaoImpl<T> {
-	protected JdbcTemplate jdbcTemplate;
-	
-	public final BaseConstant delete(long id, String tablename) {
-		int count = 0;
-		String sql = "delete from " + tablename + " where id = ?";
-		count = jdbcTemplate.update(sql,new Object[]{Long.valueOf(id)});
-		return count == 1 ? BaseStatus.SUCCESS:BaseStatus.FAILURE;
+public abstract class AbstractBaseJDBCDaoImpl<T> extends JdbcDaoSupport implements IBaseDao<T> {
+
+	protected final BaseConstant delete(long id, String sql) {
+		int count = getJdbcTemplate().update(sql, new Object[] { Long.valueOf(id) });
+		return count == 1 ? BaseStatus.SUCCESS : BaseStatus.FAILURE;
+	}
+
+	protected final BaseConstant save(String sql, Object[] objects) {
+		int count = getJdbcTemplate().update(sql, objects);
+		return count == 1 ? BaseStatus.SUCCESS : BaseStatus.FAILURE;
 	}
 }
--- lw-aggregator/lw-persistence/src/main/java/com/jarvis/lw/persistence/jdbc/UserJDBCDaoImpl.java
+++ lw-aggregator/lw-persistence/src/main/java/com/jarvis/lw/persistence/jdbc/UserJDBCDaoImpl.java
@@ -1,26 +1,93 @@
 package com.jarvis.lw.persistence.jdbc;
 
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.jarvis.lw.core.constants.BaseConstant;
 import com.jarvis.lw.core.constants.BaseStatus;
 import com.jarvis.lw.core.domain.User;
 import com.jarvis.lw.persistence.IUserDao;
 
-public class UserJDBCDaoImpl extends JdbcDaoSupport implements IUserDao {
 
+
+public class UserJDBCDaoImpl extends AbstractBaseJDBCDaoImpl<User> implements IUserDao {
 	private static final String TABLENAME = "t_user";
 	
-	public BaseConstant save(User object) {
-		BaseStatus status;
-		String sql = "insert into t_user(name) values";
-		return null;
+	public BaseConstant save(User user) {
+		String sql = "insert into t_user";
+		
+		StringBuilder colunmBuilder = new StringBuilder();
+		StringBuilder paramBuilder = new StringBuilder();
+		List objList = new ArrayList();
+		
+		if (user.getName() != null && !"".equals(user.getName())) {
+			if (colunmBuilder.toString().length() != 0) {
+				colunmBuilder.append(',');
+				paramBuilder.append(',');
+			}
+			colunmBuilder.append("name");
+			paramBuilder.append('?');
+			objList.add(user.getName());
+		}
+		
+		if (user.getAge() != null) {
+			if (colunmBuilder.toString().length() != 0) {
+				colunmBuilder.append(',');
+				paramBuilder.append('?');
+			}
+			colunmBuilder.append("age");
+			paramBuilder.append('?');
+			objList.add(user.getAge());
+		}
+
+		if (user.getCell() != null && !"".equals(user.getCell())) {
+			if (colunmBuilder.toString().length() != 0) {
+				colunmBuilder.append(',');
+				paramBuilder.append('?');
+			}
+			colunmBuilder.append("cell");
+			paramBuilder.append('?');
+			objList.add(user.getCell());
+		}
+		
+		if (user.getAddress() != null && !"".equals(user.getAddress())) {
+			if (colunmBuilder.toString().length() != 0) {
+				colunmBuilder.append(',');
+				paramBuilder.append(',');
+			}
+			colunmBuilder.append("address");
+			paramBuilder.append('?');
+			objList.add(user.getAddress());
+		}
+		
+		if (user.getBirthday() != null && user.getBirthday() != 0) {
+			if (colunmBuilder.toString().length() != 0) {
+				colunmBuilder.append(',');
+				paramBuilder.append(',');
+			}
+			colunmBuilder.append("birthday");
+			paramBuilder.append('?');
+			objList.add(user.getBirthday());
+		}
+		
+		if (user.getDescription() != null && !"".equals(user.getDescription())) {
+			if (colunmBuilder.toString().length() != 0) {
+				colunmBuilder.append(',');
+				paramBuilder.append(',');
+			}
+			colunmBuilder.append("description");
+			paramBuilder.append('?');
+			objList.add(user.getDescription());
+		}
+		sql = sql + "(" + colunmBuilder.toString() + ") values (" + paramBuilder.toString() + ")";
+		
+		return save(sql, objList.toArray());
 	}
 
 	public BaseConstant delete(long id) {
-		String sql = "delete from " + TABLENAME + " where id = ?";
-		int count = this.getJdbcTemplate().update(sql,new Object[]{Long.valueOf(id)});
-		return count == 1 ? BaseStatus.SUCCESS:BaseStatus.FAILURE;
+		String sql = "delete from t_user where id = ?";
+		return delete(id, sql);
 	}
 
 	public BaseConstant update(User object) {
