a6088abc200a14eae2739391ad273659cd3e4237
Drake Qiu <adidasqiu@hotmail.com>
2016-08-04 06:29:03 +0800

    [lw] update userDAO

--- lw-aggregator/lw-persistence/src/main/java/com/jarvis/lw/persistence/jdbc/AbstractBaseJDBCDaoImpl.java
+++ lw-aggregator/lw-persistence/src/main/java/com/jarvis/lw/persistence/jdbc/AbstractBaseJDBCDaoImpl.java
@@ -9,19 +9,19 @@ import com.jarvis.lw.persistence.IBaseDao;
 
 public abstract class AbstractBaseJDBCDaoImpl<T> extends JdbcDaoSupport implements IBaseDao<T> {
 
-	protected void delete(long id, String sql) {
+	protected final void delete(long id, String sql) {
 		getJdbcTemplate().update(sql, new Object[] { Long.valueOf(id) });
 	}
 
-	protected void save(String sql, Object[] objects, int[] types) {
+	protected final void save(String sql, Object[] objects, int[] types) {
 		getJdbcTemplate().update(sql, objects, types);
 	}
 	
-	protected void update(String sql, Object[] objects) {
+	protected final void update(String sql, Object[] objects) {
 		getJdbcTemplate().update(sql, objects);
 	}
 	
-	protected T findById(String sql, Object[] objects, RowMapper<T> rowMapper) {
+	protected final T findById(String sql, Object[] objects, RowMapper<T> rowMapper) {
 		List<T> list = getJdbcTemplate().query(sql, objects, rowMapper);
 		return list.size() > 0 ? list.get(0) : null;
 	}
